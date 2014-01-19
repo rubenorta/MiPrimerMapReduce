14fb4070117f185984ce444412ad9d6f2fefc763
Ruben Orta <ruben.orta@gmail.com>
2014-01-19 21:38:59 +0100

    Adding test support

--- ranking.py
+++ ranking.py
@@ -1,9 +1,9 @@
-from __future__ import division
 import mincemeat
 import glob
 import pprint
 import operator
-
+import datetime
+import movies
 
 #data_files = glob.glob('training_set/*')
 data_files = glob.glob('test/*')
@@ -16,34 +16,25 @@ def read_data(file_name):
                 f.close()
 
 data = dict((file_name, read_data(file_name)) for file_name in data_files)
+
 print "Data loaded"
+print datetime.datetime.now()
 
 def mapfn(key, value):
-
-        lines = value.splitlines()
-        for line in lines[1:]:
-                items = line.split(",")
-                film_id = items[0]
-                rating = items[1]
-                date = items[2]
-                yield film_id, rating
+    lines = value.splitlines()
+    film_id = lines[0][:-1]
+    for line in lines[1:]:
+        items = line.split(",")
+        user_id = items[0]
+        rating = items[1]
+        date = items[2]
+    yield user_id, film_id
 
 def reducefn(key, values):
-
-        #data = {}
-        values_sum = 0
-        elements = 0
-        rating = 0
-        print "====="
-        print key
+        number_of_films = 0
         for value in values:
-                values_sum += int(value)
-                elements += 1
-        rating = (values_sum / elements)
-        print values_sum
-        print elements
-        print rating
-        return rating
+                number_of_films += 1
+        return number_of_films
 
 s = mincemeat.Server()
 s.datasource = data
@@ -51,9 +42,7 @@ s.mapfn = mapfn
 s.reducefn = reducefn
 
 results = s.run_server (password = "ruben")
-
-#print results['1428688']
 sorted_results = sorted(results.iteritems(), key=operator.itemgetter(1))
 pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(results)
 pp.pprint(sorted_results)
+print datetime.datetime.now()
